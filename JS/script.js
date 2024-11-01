7a8aa853aa2c1705cd8d54d6e97dd184bc3addfe
rcardoramos <rcardoramos26@gmail.com>
2024-11-01 00:58:38 -0500

    Solución active navbar - modificación color

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,20 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-links li');
+
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
+    document.body.classList.toggle('no-scroll', navLinks.classList.contains('active'));
+
 }
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+    });
+});
 
 document.querySelectorAll('.nav-links a').forEach(link => {
     link.addEventListener('click', () => {
